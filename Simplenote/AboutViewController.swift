e3a3851a5821215afc8570b5d4ff894247d0014c
Dan Roundhill <dan@a8c.com>
2018-02-13 15:37:54 -0800

    Better swift syntax using a constants struct and unwrapped optionals for the IBOutlets.

--- Simplenote/AboutViewController.swift
+++ Simplenote/AboutViewController.swift
@@ -6,87 +6,89 @@
 import Cocoa
 
 class AboutViewController: NSViewController {
-    let blogUrl =       "https://simplenote.com/blog"
-    let twitterUrl =    "https://twitter.com/simplenoteapp"
-    let appsUrl =       "https://simplenote.com"
-    let githubUrl =     "https://github.com/automattic/simplenote-macos"
-    let hiringUrl =     "https://automattic.com/work-with-us"
-    let privacyUrl =    "https://simplenote.com/privacy/"
-    let termsUrl =      "https://simplenote.com/terms/"
+    struct Constants {
+        static let blogUrl =       "https://simplenote.com/blog"
+        static let twitterUrl =    "https://twitter.com/simplenoteapp"
+        static let appsUrl =       "https://simplenote.com"
+        static let githubUrl =     "https://github.com/automattic/simplenote-macos"
+        static let hiringUrl =     "https://automattic.com/work-with-us"
+        static let privacyUrl =    "https://simplenote.com/privacy/"
+        static let termsUrl =      "https://simplenote.com/terms/"
+    }
     
-    @IBOutlet var blogLabel:        SPAboutTextField?
-    @IBOutlet var twitterLabel:     SPAboutTextField?
-    @IBOutlet var appsLabel:        SPAboutTextField?
-    @IBOutlet var githubLabel:      SPAboutTextField?
-    @IBOutlet var hiringLabel:      SPAboutTextField?
-    @IBOutlet var privacyLabel:     SPAboutTextField?
-    @IBOutlet var tosLabel:         SPAboutTextField?
-    @IBOutlet var versionLabel:     NSTextField?
-    @IBOutlet var copyrightLabel:   NSTextField?
+    @IBOutlet var blogLabel:        SPAboutTextField!
+    @IBOutlet var twitterLabel:     SPAboutTextField!
+    @IBOutlet var appsLabel:        SPAboutTextField!
+    @IBOutlet var githubLabel:      SPAboutTextField!
+    @IBOutlet var hiringLabel:      SPAboutTextField!
+    @IBOutlet var privacyLabel:     SPAboutTextField!
+    @IBOutlet var tosLabel:         SPAboutTextField!
+    @IBOutlet var versionLabel:     NSTextField!
+    @IBOutlet var copyrightLabel:   NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let blogClick = NSClickGestureRecognizer(target: self, action: #selector(blogLabelClick))
-        blogLabel?.addGestureRecognizer(blogClick)
+        blogLabel.addGestureRecognizer(blogClick)
         
         let twitterClick = NSClickGestureRecognizer(target: self, action: #selector(twitterLabelClick))
-        twitterLabel?.addGestureRecognizer(twitterClick)
+        twitterLabel.addGestureRecognizer(twitterClick)
         
         let appsClick = NSClickGestureRecognizer(target: self, action: #selector(appsLabelClick))
-        appsLabel?.addGestureRecognizer(appsClick)
+        appsLabel.addGestureRecognizer(appsClick)
         
         let githubClick = NSClickGestureRecognizer(target: self, action: #selector(githubLabelClick))
-        githubLabel?.addGestureRecognizer(githubClick)
+        githubLabel.addGestureRecognizer(githubClick)
         
         let hiringClick = NSClickGestureRecognizer(target: self, action: #selector(hiringLabelClick))
-        hiringLabel?.addGestureRecognizer(hiringClick)
+        hiringLabel.addGestureRecognizer(hiringClick)
         
         let privacyClick = NSClickGestureRecognizer(target: self, action: #selector(privacyLabelClick))
-        privacyLabel?.addGestureRecognizer(privacyClick)
+        privacyLabel.addGestureRecognizer(privacyClick)
         
         let termsClick = NSClickGestureRecognizer(target: self, action: #selector(termsLabelClick))
-        tosLabel?.addGestureRecognizer(termsClick)
+        tosLabel.addGestureRecognizer(termsClick)
         
         // Display app version in the header
         let dictionary = Bundle.main.infoDictionary!
         let version = dictionary["CFBundleShortVersionString"] as! String
         let versionString = NSLocalizedString("Version %@", comment: "Version displayed in the about screen")
-        versionLabel?.stringValue = String(format: versionString, version)
+        versionLabel.stringValue = String(format: versionString, version)
         
         // Display the current year in the copyright footer
         let date = Date()
         let calendar = Calendar.current
         let thisYear = calendar.component(.year, from: date)
         let copyrightText = String(format: "© %d Automattic", thisYear) // No need for translation
-        copyrightLabel?.stringValue = copyrightText
+        copyrightLabel.stringValue = copyrightText
     }
     
     @objc func blogLabelClick() {
-        NSWorkspace.shared.open(URL(string: blogUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.blogUrl)!)
     }
     
     @objc func twitterLabelClick() {
-        NSWorkspace.shared.open(URL(string: twitterUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.twitterUrl)!)
     }
     
     @objc func appsLabelClick() {
-        NSWorkspace.shared.open(URL(string: appsUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.appsUrl)!)
     }
     
     @objc func githubLabelClick() {
-        NSWorkspace.shared.open(URL(string: githubUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.githubUrl)!)
     }
     
     @objc func hiringLabelClick() {
-        NSWorkspace.shared.open(URL(string: hiringUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.hiringUrl)!)
     }
     
     @objc func privacyLabelClick() {
-        NSWorkspace.shared.open(URL(string: privacyUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.privacyUrl)!)
     }
     
     @objc func termsLabelClick() {
-        NSWorkspace.shared.open(URL(string: termsUrl)!)
+        NSWorkspace.shared.open(URL(string: Constants.termsUrl)!)
     }
 }
