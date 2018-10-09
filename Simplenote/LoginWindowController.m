dfd6035c90e826dcc3c55dc7501d3dc179e8fe5f
Dan Roundhill <dan@a8c.com>
2018-10-09 16:44:12 -0700

    Enforce the light aqua appearance for the login window.

--- Simplenote/LoginWindowController.m
+++ Simplenote/LoginWindowController.m
@@ -19,6 +19,11 @@ static NSString *SPAuthSessionKey                   = @"SPAuthSessionKey";
 - (instancetype)init {
     self = [super init];
     
+    // We want the login window to always have the 'light' aqua appearance
+    if (@available(macOS 10.14, *)) {
+        self.window.appearance = [NSAppearance appearanceNamed:NSAppearanceNameAqua];
+    }
+    
     // Sanity check for accessing the root view
     if (self.window.contentView.subviews.count < 1) {
         return self;
