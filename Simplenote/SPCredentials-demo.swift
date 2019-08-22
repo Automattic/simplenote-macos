11fe2f1765a7d24d0e254cbf189ef2cc94cfcff8
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-08-22 20:48:32 -0300

    Adds SPCredentials-demo

---
+++ Simplenote/SPCredentials-demo.swift
@@ -0,0 +1,37 @@
+/// Simplenote API Demo Credentials
+///
+@objcMembers
+class SPCredentials: NSObject {
+
+    /// BitHockey
+    ///
+    static let bitHockeyIdentifier = ""
+
+    /// Sentry DSN
+    ///
+    static let sentryDSN = "https://not.required"
+
+    /// Simperium: AppID
+    ///
+    static let simperiumAppID = "history-analyst-dad"
+
+    /// Simperium: API Key
+    ///
+    static let simperiumApiKey = "1c40d66b2ddd422eb799a68518263360"
+
+    /// Simperium: Preferences Object Key
+    ///
+    static let simperiumPreferencesObjectKey = "not-required"
+
+    /// Simperium: Settings Object Key
+    ///
+    static let simperiumSettingsObjectKey = "not-required"
+
+    /// WordPressSSO: Client ID
+    ///
+    static let wpcomClientID = "not-required"
+
+    /// WordPressSSO: Redirect URL
+    ///
+    static let wpcomRedirectURL = "not-required"
+}
