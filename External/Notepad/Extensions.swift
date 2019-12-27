f29bbaeb7354eb090e701194e0a5d6256fd59991
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-12-27 14:53:48 -0300

    String: Nukes unused API

--- External/Notepad/Extensions.swift
+++ External/Notepad/Extensions.swift
@@ -30,20 +30,4 @@ extension String {
 
         return pattern
     }
-
-    /// Converts a NSRange to a Range<String.Index>.
-    /// http://stackoverflow.com/a/30404532/6669540
-    ///
-    /// - parameter range: The NSRange.
-    ///
-    /// - returns: The equivalent Range<String.Index>.
-    func range(from nsRange: NSRange) -> Range<String.Index>? {
-        guard
-            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
-            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
-            let from = String.Index(from16, within: self),
-            let to = String.Index(to16, within: self)
-            else { return nil }
-        return from ..< to
-    }
 }
