1bdf41b09ec1c23422822ece472f0abe4571fa9e
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-12-27 16:35:37 -0300

    NSAttributedString: New extension API

--- Simplenote/NSAttributedString+Simplenote.swift
+++ Simplenote/NSAttributedString+Simplenote.swift
@@ -10,4 +10,12 @@ extension NSAttributedString {
     var rangeOfEntireString: NSRange {
         return NSRange(location: 0, length: length)
     }
+
+    /// Nukes the specified attributes from a given range
+    ///
+    func removeAttributes(_ names: [NSAttributedString.Key], range: NSRange) {
+        for name in names {
+            removeAttribute(name, range: range)
+        }
+    }
 }
