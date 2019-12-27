b15db5c0aaae811375bc1058298d22260d91d138
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-12-27 14:53:42 -0300

    UniversalColor: Nukes unused API

--- External/Notepad/Extensions.swift
+++ External/Notepad/Extensions.swift
@@ -14,29 +14,6 @@ import Foundation
     import struct AppKit.CGFloat
 #endif
 
-extension UniversalColor {
-    /// Converts a hex color code to UIColor.
-    /// http://stackoverflow.com/a/33397427/6669540
-    ///
-    /// - parameter hexString: The hex code.
-    convenience init(hexString: String) {
-        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int = UInt32()
-        Scanner(string: hex).scanHexInt32(&int)
-        let a, r, g, b: UInt32
-        switch hex.count {
-            case 3: // RGB (12-bit)
-                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-            case 6: // RGB (24-bit)
-                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-            case 8: // ARGB (32-bit)
-                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-            default:
-                (a, r, g, b) = (255, 0, 0, 0)
-        }
-        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
-    }
-}
 
 extension String {
     /// Converts a String to a NSRegularExpression.
