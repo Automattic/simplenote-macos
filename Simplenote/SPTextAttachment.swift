2589b4e3221e29a7acb80f4da3f6eaad25ba0e8a
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-01-03 13:17:17 -0300

    SPTextAttachment: macOS < Catalina should invert the image

--- Simplenote/SPTextAttachment.swift
+++ Simplenote/SPTextAttachment.swift
@@ -23,7 +23,24 @@ import Foundation
         set(isChecked) {
             checked = isChecked
             let name = checked ? "icon_task_checked" : "icon_task_unchecked"
-            image = NSImage(named: name)?.colorized(with: attachmentColor)
+            guard let asset = NSImage(named: name)?.colorized(with: attachmentColor) else {
+                fatalError()
+            }
+
+            if #available(macOS 10.15, *) {
+                image = asset
+                return
+            }
+
+            // macOS < 10.15 inverts the NSTextAttachment.image when drawn. So cool.
+            // References:
+            //  -   https://stackoverflow.com/questions/49649553/the-image-of-nstextattachment-is-flipped?noredirect=1&lq=1
+            //  -   https://github.com/Automattic/simplenote-macos/pull/403
+            //
+            image = NSImage(size: asset.size, flipped: true) { rect in
+                asset.draw(in: rect, from: .zero, operation: .sourceOver, fraction: 1.0)
+                return true
+            }
         }
     }
 }
