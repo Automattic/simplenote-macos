282915672df210dd94b6094f7a66ce5678f211d3
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-12 15:30:52 -0300

    NSAttributedStringToMarkdownConverterTests: Updates Unit Test

--- SimplenoteTests/NSAttributedStringToMarkdownConverterTests.swift
+++ SimplenoteTests/NSAttributedStringToMarkdownConverterTests.swift
@@ -52,14 +52,14 @@ private extension NSAttributedStringToMarkdownConverterTests {
     }
 
     func documentWithoutAttachments() -> NSAttributedString {
-        let sample =    """
-                        orem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquid ex ea commodi consequat.
-                        Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt.
-                        """
+        let sample = """
+                     orem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                     nisi ut aliquid ex ea commodi consequat.
+                     Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                     Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt.
+                     """
 
         return NSAttributedString(string: sample)
     }
