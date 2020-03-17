51e1ced44a2e66c5a281c2a7f4a5476f435cee82
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-17 18:01:54 -0300

    NSTextView+Simplenote: New API

--- Simplenote/NSTextView+Simplenote.swift
+++ Simplenote/NSTextView+Simplenote.swift
@@ -15,4 +15,18 @@ extension NSTextView {
     func processNewlineInsertion() -> Bool {
         return true
     }
+
+    /// Remove the text at the specified range, and notifies the delegate.
+    ///
+    func removeText(at range: NSRange) {
+        insertText(String(), replacementRange: range)
+        notifyTextViewDidChange()
+    }
+
+    /// Notifies the delegate that the Text was updated
+    ///
+    private func notifyTextViewDidChange() {
+        let note = Notification(name: NSText.didChangeNotification)
+        delegate?.textDidChange?(note)
+    }
 }
--- SimplenoteTests/NSTextViewSimplenoteTests.swift
+++ SimplenoteTests/NSTextViewSimplenoteTests.swift
@@ -5,4 +5,27 @@ import XCTest
 // MARK: - NSAttributedStringToMarkdownConverter Unit Tests
 //
 class NSTextViewSimplenoteTests: XCTestCase {
+
+    /// TextView!
+    ///
+    private var textView: NSTextView!
+
+
+    // MARK: - Overridden Methods
+
+    override func setUp() {
+        textView = NSTextView()
+    }
+    /// Verifies that `removeText(at:)` effectively nukes the text at the specified range
+    ///
+    func testRemoveTextNukesSpecifiedRangeAndNotifiesDelegate() {
+        let sample = "- L1\n- L2\n- L3"
+        let expected = "- L1\n- L3"
+        let targetRange = NSRange(location: 5, length: 5)
+
+        textView.string = sample
+        textView.removeText(at: targetRange)
+
+        XCTAssertEqual(textView.string, expected)
+    }
 }
