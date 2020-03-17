0b94c087978d555fbbc455612cdc415327e20386
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-17 16:17:39 -0300

    NSTextView: New Simplenote Extension

---
+++ Simplenote/NSTextView+Simplenote.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+// MARK: - Simplenote API
+//
+extension NSTextView {
+    @objc
+    func processTabInsertion() -> Bool {
+        return true
+    }
+
+    ///
+    ///
+    @objc
+    func processNewlineInsertion() -> Bool {
+        return true
+    }
---
+++ SimplenoteTests/NSTextViewSimplenoteTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Simplenote
+
+
+// MARK: - NSAttributedStringToMarkdownConverter Unit Tests
+//
+class NSTextViewSimplenoteTests: XCTestCase {
+}
