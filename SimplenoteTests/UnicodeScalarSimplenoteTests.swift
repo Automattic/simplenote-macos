60cc04de86ac819dd55fc0917198fb70499e7ea0
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-17 13:32:59 -0300

    Implements UnicodeScalar Extension

---
+++ SimplenoteTests/UnicodeScalarSimplenoteTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import Simplenote
+
+
+// MARK: - UnicodeScalarSimplenoteTests Unit Tests
+//
+class UnicodeScalarSimplenoteTests: XCTestCase {
+
+    /// Verifies that `isWhitespace` returns true for Spaces and Tabs
+    ///
+    func testIsWhitespaceReturnsTrueForSpacesAndTabs() {
+        XCTAssertTrue(UnicodeScalar(" ").isWhitespace)
+        XCTAssertTrue(UnicodeScalar("\t").isWhitespace)
+    }
+
+    /// Verifies that `isWhitespace` returns false for alphanumeric characters
+    ///
+    func testIsWhitespaceReturnsFalseForAlphanumericCharacters() {
+        for character in UnicodeScalar("a").value...UnicodeScalar("z").value {
+            XCTAssert(UnicodeScalar(character)?.isWhitespace == false)
+        }
+
+        for character in UnicodeScalar("A").value...UnicodeScalar("Z").value {
+            XCTAssert(UnicodeScalar(character)?.isWhitespace == false)
+        }
+
+        for character in UnicodeScalar("0").value...UnicodeScalar("9").value {
+            XCTAssert(UnicodeScalar(character)?.isWhitespace == false)
+        }
+    }
+
+}
