89dd1501ab364248afe120ba35f5a24112368b98
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-20 17:56:22 -0300

    NSMutableAttributedString: New Simplenote API

--- Simplenote/NSMutableAttributedString+Simplenote.swift
+++ Simplenote/NSMutableAttributedString+Simplenote.swift
@@ -40,4 +40,16 @@ extension NSMutableAttributedString {
         let string = NSAttributedString(string: String(character))
         append(string)
     }
+
+    /// Enumerates all of the NSTextAttachment(s) of the specified kind
+    ///
+    func enumerateAttachments<T: NSTextAttachment>(of type: T.Type, closure: (_ attachment: T, _ range: NSRange) -> Void) {
+        enumerateAttribute(.attachment, in: fullRange, options: .reverse) { (payload, range, _) in
+            guard let attachment = payload as? T else {
+                return
+            }
+
+            closure(attachment, range)
+        }
+    }
 }
---
+++ SimplenoteTests/TestConstants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+/// Unit Tests Constants
+///
+struct TestConstants {
+
+    /// Default Expectation Timeout
+    ///
+    static let defaultTimeout = TimeInterval(10)
+}
