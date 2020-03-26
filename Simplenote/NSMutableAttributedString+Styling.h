0bd04af0b2c1139703b545e73caf713fd3810957
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-26 14:12:18 -0300

    NSMutableAttributedString+Styling: Nullability Specifier

--- Simplenote/NSMutableAttributedString+Styling.h
+++ Simplenote/NSMutableAttributedString+Styling.h
@@ -10,8 +10,12 @@
 
 @class SPTextAttachment;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface NSMutableAttributedString (Styling)
 
 - (NSArray<SPTextAttachment *> *)processChecklistsWithColor:(NSColor *)color;
 
 @end
+
+NS_ASSUME_NONNULL_END
