d26d29c644e3defbcf69b7c87f520991c389e6f8
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-03-26 19:58:08 -0300

    NSMutableAttributedString+Simplenote: New APIs

--- Simplenote/NSMutableAttributedString+Simplenote.swift
+++ Simplenote/NSMutableAttributedString+Simplenote.swift
@@ -34,3 +34,37 @@ extension NSMutableAttributedString {
         append(string)
     }
 }
+
+
+// MARK: - Text Replacement
+//
+extension NSMutableAttributedString {
+
+    /// Replaces the *FULL* contents of the receiver with the specified AttributedString. We also register the inverse OP in the UndoManager
+    ///
+    @objc(replaceCharactersWithAttrString:undoManager:)
+    func replaceCharacters(with attrString: NSAttributedString, undoManager: UndoManager) {
+        let undoString = attributedSubstring(from: fullRange)
+        let undoRange = attrString.fullRange
+
+        undoManager.registerUndo(withTarget: self) { _ in
+            self.replaceCharacters(in: undoRange, with: undoString)
+        }
+
+        replaceCharacters(in: fullRange, with: attrString)
+    }
+
+    /// Applies the changes encapsualted in a specified array of Ranges and Strings
+    ///
+    func replaceCharacters(in ranges: [NSValue], with strings: [String]) -> Bool {
+        guard ranges.count == strings.count else {
+            return false
+        }
+
+        for (value, string) in zip(ranges, strings).reversed() {
+            replaceCharacters(in: value.rangeValue, with: string)
+        }
+
+        return true
+    }
+}
