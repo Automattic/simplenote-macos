c7da687714e6d411788aab5325fb77eecccc5214
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-01 14:06:32 -0300

    NSTextView+Simplenote: Cleanup

--- Simplenote/NSTextView+Simplenote.swift
+++ Simplenote/NSTextView+Simplenote.swift
@@ -23,6 +23,12 @@ extension NSTextView {
         }
     }
 
+    /// Removes the text at the specified range, and notifies the delegate.
+    ///
+    func removeText(at range: NSRange) {
+        insertText(String(), replacementRange: range)
+    }
+
     /// Returns the (Range, String) representing the line or lines at the Selected Range.
     ///
     /// - Important: The trailing `\n` will be dropped from both, the resulting String and Range.
@@ -34,12 +40,6 @@ extension NSTextView {
         let trimmedRange = NSRange(location: range.location, length: trimmedLine.utf16.count)
 
         return (trimmedRange, trimmedLine)
-    }
-
-    /// Removes the text at the specified range, and notifies the delegate.
-    ///
-    func removeText(at range: NSRange) {
-        insertText(String(), replacementRange: range)
     }
 }
 
