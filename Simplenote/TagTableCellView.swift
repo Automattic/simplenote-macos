3e581ad12665dc3ea7c218aefb2efb908fbd07ce
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-16 17:12:43 -0300

    TagTableCellView: Tracking Area Support

--- Simplenote/TagTableCellView.swift
+++ Simplenote/TagTableCellView.swift
@@ -11,8 +11,45 @@ class TagTableCellView: NSTableCellView {
     ///
     private(set) var mouseInside = false
 
+    ///
+    ///
+    private lazy var trackingArea: NSTrackingArea = {
+        NSTrackingArea(rect: .zero, options: [.inVisibleRect, .activeAlways, .mouseEnteredAndExited], owner: self, userInfo: nil)
+    }()
+
+
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        mouseInside = false
+        applyStyle()
+    }
 
     func applyStyle() {
 
+    }
+}
+
+
+// MARK: - Tracking Areas
+//
+extension TagTableCellView {
+
+    override func updateTrackingAreas() {
+        super.updateTrackingAreas()
+        guard trackingAreas.contains(trackingArea) == false else {
+            return
+        }
+        addTrackingArea(trackingArea)
+    }
+
+    override func mouseEntered(with event: NSEvent) {
+        mouseInside = true
+        NSCursor.pointingHand.set()
+    }
+
+    override func mouseExited(with event: NSEvent) {
+        mouseInside = false
+        NSCursor.arrow.set()
     }
 }
