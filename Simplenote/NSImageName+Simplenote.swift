b2d735ad90c499e1d70613b691a06ec8b701fa59
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-16 16:37:27 -0300

    Implements NSImage.Name Extension

---
+++ Simplenote/NSImageName+Simplenote.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+// MARK: - NSImage.Name
+//
+extension NSImage.Name {
+
+    /// All Notes Icon, meant to be used in the Tags List
+    ///
+    static let allNotes = NSImage.Name("button_all_notes")
+
+    /// Trash Icon, meant to be used in the Tags List
+    ///
+    static let trash = NSImage.Name("button_trash")
+}
