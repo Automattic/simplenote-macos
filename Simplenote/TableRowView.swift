94f1473eff64811ab3d078cf871a583c2fb2dc14
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-20 16:44:42 -0300

    Revert "TableRowView: Implements TableCellView"
    This reverts commit 02016e42eee99a2635fa33840b66f696b15edec1.

--- Simplenote/TableRowView.swift
+++ Simplenote/TableRowView.swift
@@ -1,14 +1,6 @@
 import Foundation
 
 
-// MARK: - TableCellView: In AppKit several details (such as the cell's selection state) are handled via TableRowViews.
-//                        We'll rely on this mechanism to forward the selection state
-//
-protocol TableCellView: class {
-    var isSelected: Bool { get set }
-}
-
-
 // MARK: - TableRowView
 //
 @objcMembers
@@ -17,25 +9,6 @@ class TableRowView : NSTableRowView {
     /// Background Color to be applied whenever the Row is selected
     ///
     var selectedBackgroundColor: NSColor?
-
-
-    /// We're overriding `isSelected` so that we can forward any seleciton events to the associated TableCellView
-    ///
-    override var isSelected: Bool {
-        didSet {
-            cellView?.isSelected = isSelected
-        }
-    }
-
-    /// Returns the associated `TableCellView`, if any
-    ///
-    private var cellView: TableCellView? {
-        for case let targetView as TableCellView in subviews {
-            return targetView
-        }
-
-        return nil
-    }
 
 
     // MARK: - Overridden
