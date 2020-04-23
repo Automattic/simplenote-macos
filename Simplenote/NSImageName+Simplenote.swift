0a5c05660c4011c8f5a09c5e98116319f32a52f9
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-23 18:11:18 -0300

    Revert "New NSImageName"
    This reverts commit e8e15c5337d9fd5a24637afed32c820d99d878e8.

--- Simplenote/NSImageName+Simplenote.swift
+++ Simplenote/NSImageName+Simplenote.swift
@@ -9,10 +9,6 @@ extension NSImage.Name {
     ///
     static let allNotes = NSImage.Name("button_all_notes")
 
-    /// New Note Icon, meant to be used in the Notes List
-    ///
-    static let newNote = NSImage.Name("button_new_note")
-
     /// Trash Icon, meant to be used in the Tags List
     ///
     static let trash = NSImage.Name("button_trash")
