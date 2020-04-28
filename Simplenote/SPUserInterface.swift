140adfd4ca15b970ba0c7b461fa26537e3854a40
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-28 16:31:07 -0300

    SPUserInterface: Drops VSThemeManager

--- Simplenote/SPUserInterface.swift
+++ Simplenote/SPUserInterface.swift
@@ -4,19 +4,41 @@ import AppKit
 
 // MARK: - Simplenote's Theme
 //
-@objc
+@objcMembers
 class SPUserInterface: NSObject {
 
     /// Ladies and gentlemen, this is a singleton.
     ///
-    @objc
     static let shared = SPUserInterface()
 
     /// Indicates if the User Interface is in Dark Mode
     ///
-    @objc
     static var isDark: Bool {
-        // TODO: Single spot to check appearance please
-        return VSThemeManager.shared().isDarkMode()
+        guard isSystemThemeSelected else {
+            return Options.shared.theme == "dark"
+        }
+
+        return isSystemInDarkMode
+    }
+
+    /// Indicates if the System Theme is selected: Starting from +10.14, whenever the theme's name is nil
+    ///
+    static var isSystemThemeSelected: Bool {
+        guard #available(macOS 10.14, *) else {
+            return false
+        }
+
+        return Options.shared.theme == nil
+    }
+
+    /// Indicates if macOS is in Dark Mode
+    ///
+    static var isSystemInDarkMode: Bool {
+        guard #available(macOS 10.14, *) else {
+            return false
+        }
+
+        return UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
     }
 }
+
