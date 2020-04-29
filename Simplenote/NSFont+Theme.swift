254c2b790849276677684f4b727f6d443bc1cbd8
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-04-29 18:54:19 -0300

    Implements NSFont Extension

---
+++ Simplenote/NSFont+Theme.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+
+// MARK: - NSFont + Theme API
+//
+extension NSFont {
+
+    @objc
+    static var simplenotePopoverTextFont: NSFont {
+        .systemFont(ofSize: 13)
+    }
+}
