4edc58e657cf04c1304c737b551d34d82ca97108
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-07 16:29:40 -0300

    New AAPL Notification

--- Simplenote/NSNotification+Simplenote.h
+++ Simplenote/NSNotification+Simplenote.h
@@ -1,6 +1,11 @@
 #import <Foundation/Foundation.h>
 
 
+// MARK: - AAPL's Theme Notifications
+//
+extern NSString * const AppleInterfaceThemeChangedNotification;
+
+
 // MARK: - Simplenote Notifications: Someone forgot to bridge NSNotification.Name over to ObjC. =(
 //
 extern NSString * const ThemeDidChangeNotification;
--- Simplenote/NSNotification+Simplenote.m
+++ Simplenote/NSNotification+Simplenote.m
@@ -1,6 +1,10 @@
 #import "NSNotification+Simplenote.h"
 
 
+// MARK: - AAPL's Theme Notifications
+//
+NSString * const AppleInterfaceThemeChangedNotification = @"AppleInterfaceThemeChangedNotification";
+
 // MARK: - Simplenote's Notifications
 //
 NSString * const ThemeDidChangeNotification = @"ThemeDidChangeNotification";
