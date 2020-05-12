ae7780a51230ec32a9c55f58f93a5ececd8ec422
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-12 17:23:08 -0300

    AppDelegate: Restores NSSplitViewDelegate Conformance

--- Simplenote/SimplenoteAppDelegate.h
+++ Simplenote/SimplenoteAppDelegate.h
@@ -19,7 +19,7 @@
 #pragma mark SimplenoteAppDelegate
 #pragma mark ====================================================================================
 
-@interface SimplenoteAppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+@interface SimplenoteAppDelegate : NSObject <NSApplicationDelegate, NSSplitViewDelegate, NSWindowDelegate>
 
 @property (strong, nonatomic, readonly) IBOutlet NSWindow                 *window;
 
