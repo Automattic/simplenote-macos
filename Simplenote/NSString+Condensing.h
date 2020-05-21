4eda943509310c3aa9d80c7353bdf47f2f13acc9
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-21 18:19:36 -0300

    NSString+Condensing: Restores legacy API

--- Simplenote/NSString+Condensing.h
+++ Simplenote/NSString+Condensing.h
@@ -14,6 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 - (NSString *)stringByReplacingNewlinesWithSpaces;
 
+- (NSString *)stringByGeneratingPreview;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Simplenote/NSString+Condensing.m
+++ Simplenote/NSString+Condensing.m
@@ -1,5 +1,7 @@
 #import "NSString+Condensing.h"
 
+#define kMaxPreviewLength 500
+
 @implementation NSString (Condensing)
 
 - (void)generatePreviewStrings:(void (^)(NSString *titlePreview, NSString *bodyPreview))block
@@ -40,6 +42,49 @@
     [components removeObject:@""];
 
     return [components componentsJoinedByString:@" "];
+}
+
+- (NSString *)stringByGeneratingPreview
+{
+    NSString *aString = [NSString stringWithString:self];
+    NSString *titlePreview;
+    NSString *contentPreview;
+
+    // Optimize to make sure a bunch of text doesn't get rendered but clipped in the previews
+    if (aString.length > kMaxPreviewLength) {
+        aString = [aString substringToIndex:kMaxPreviewLength];
+    }
+
+    NSString *contentTest = [aString stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+
+    NSRange firstNewline = [contentTest rangeOfString: @"\n"];
+    if (firstNewline.location == NSNotFound) {
+        titlePreview = contentTest;
+        contentPreview = nil;
+    } else {
+        titlePreview = [contentTest substringToIndex:firstNewline.location];
+        contentPreview = [[contentTest substringFromIndex: firstNewline.location+1] stringByReplacingOccurrencesOfString:@"\n\n" withString:@" \n"];
+
+        // Remove leading newline if applicable
+        NSRange nextNewline = [contentPreview rangeOfString: @"\n"];
+        if (nextNewline.location == 0) {
+            contentPreview = [contentPreview substringFromIndex:1];
+        }
+    }
+
+    // Remove Markdown #'s
+    if ([titlePreview hasPrefix:@"#"]) {
+        NSRange cutRange = [titlePreview rangeOfString:@"# "];
+        if (cutRange.location != NSNotFound) {
+            titlePreview = [titlePreview substringFromIndex:cutRange.location + cutRange.length];
+        }
+    }
+
+    if (contentPreview) {
+        return [NSString stringWithFormat:@"%@\n%@", titlePreview, contentPreview];
+    }
+
+    return titlePreview;
 }
 
 @end
