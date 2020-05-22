127c9be285b9de41d404ac2090f566f36e740a38
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-22 16:25:13 -0300

    Implements BackgroundView

---
+++ Simplenote/BackgroundView.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+
+// MARK: - BackgroundView
+//
+class BackgroundView: NSView {
+
+    /// Bottom Border: Color
+    ///
+    var bottomBorderColor: NSColor? = .simplenoteDividerColor
+
+    /// Bottom Border: Width
+    ///
+    var bottomBorderWidth = NSScreen.main?.pointToPixelRatio
+
+    /// Fill Color
+    ///
+    var fillColor: NSColor?
+
+
+    // MARK: - Overridden Methods
+
+    override func draw(_ dirtyRect: NSRect) {
+        if let fillColor = fillColor {
+            fillColor.set()
+            NSBezierPath(rect: dirtyRect).fill()
+        }
+
+        if let bottomBorderWidth = bottomBorderWidth, let bottomBorderColor = bottomBorderColor {
+            let bottomRect = NSRect(x: .zero, y: .zero, width: dirtyRect.width, height: bottomBorderWidth)
+            bottomBorderColor.set()
+            NSBezierPath(rect: bottomRect).fill()
+        }
+    }
+}
