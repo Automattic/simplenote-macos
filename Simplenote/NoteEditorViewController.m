93b99afe806ab0c80832e3e1450a4a36daee03ee
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-29 11:18:47 -0300

    NoteEditorViewController: Drops WKNavigationDelegate conformance

--- Simplenote/NoteEditorViewController.m
+++ Simplenote/NoteEditorViewController.m
@@ -53,8 +53,7 @@ static NSInteger const SPVersionSliderMaxVersions       = 30;
                                         NSTokenFieldDelegate,
                                         PublishViewControllerDelegate,
                                         SPBucketDelegate,
-                                        VersionsViewControllerDelegate,
-                                        WKNavigationDelegate>
+                                        VersionsViewControllerDelegate>
 
 @property (nonatomic,   weak) VersionsViewController    *versionsViewController;
 @property (nonatomic,   weak) ShareViewController       *shareViewController;
