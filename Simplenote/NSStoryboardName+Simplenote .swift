e23b3ddef91972b8a75b9104f55f473f02194a4e
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-06-25 18:48:56 -0300

    Implements NSStoryboard.Name + Simplenote

---
+++ Simplenote/NSStoryboardName+Simplenote .swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+// MARK: - NSStoryboard.Name Simplenote Extension
+//
+extension NSStoryboard.Name {
+
+    /// Metrics Storyboard: Displays Note Statistics
+    ///
+    static var metrics = NSStoryboard.Name(stringLiteral: "Metrics")
+}
