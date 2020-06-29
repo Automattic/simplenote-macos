31f08dc99c0c86cf477e2a6bae95dd20a2999ab0
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-06-29 17:12:52 -0300

    MetricsController: Observing for changes

--- Simplenote/MetricsController.swift
+++ Simplenote/MetricsController.swift
@@ -19,12 +19,23 @@ class MetricsController {
     ///
     var onChange: (() -> Void)?
 
+    // MARK: - Lifecycle
+
+    deinit {
+        stopListeningForNotifications()
+    }
+
+    init() {
+        startListeningForNotifications()
+    }
+
+
+    // MARK: - Public API
 
     /// Starts observing changes over a given collection of Notes, and invokes `onChange` when any of such entities is updated.
     /// - Note: The very first time this API is invoked, it'll call `onChange` back
     ///
     func startReportingMetrics(for notes: [Note]) {
-// TODO: Start listening for changes
         self.notes = notes
         onChange?()
     }
@@ -63,5 +74,67 @@ extension MetricsController {
     ///
     var numberOfWords: Int {
         notes.isEmpty ? .zero : NSSpellChecker.shared.countWords(in: contents, language: nil)
+    }
+}
+
+
+// MARK: - Notification Helpers
+//
+private extension MetricsController {
+
+    func startListeningForNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(mainContextWasUpdated),
+                                               name: .NSManagedObjectContextObjectsDidChange,
+                                               object: SimplenoteAppDelegate.shared().managedObjectContext)
+    }
+
+    func stopListeningForNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+
+// MARK: - Listening for Changes!
+//
+private extension MetricsController {
+
+    /// Whenever a `NSManagedObjectContextObjectsDidChange` arrives, and any of the `notes` entities we're observing gets updated,
+    /// we'll dispatch an `onChange()` invocation, notifying our observers that there are new metrics available.
+    ///
+    @objc
+    func mainContextWasUpdated(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+
+        let observedKeys = [NSUpdatedObjectsKey, NSRefreshedObjectsKey]
+        let updatedObjectIDs = extractObjectIdentifiers(from: userInfo, keys: observedKeys)
+        let observedObjectIDs = notes.map { $0.objectID }
+
+        guard !updatedObjectIDs.intersection(observedObjectIDs).isEmpty else {
+            return
+        }
+
+        DispatchQueue.main.async { [weak self] in
+            self?.onChange?()
+        }
+    }
+
+    /// Given a Notification's Payload, this API will extract the collection of NSManagedObjectID(s) stored under the specified keys.
+    ///
+    func extractObjectIdentifiers(from userInfo: [AnyHashable: Any], keys: [String]) -> Set<NSManagedObjectID> {
+        var output = Set<NSManagedObjectID>()
+
+        for key in keys {
+            guard let objects = userInfo[key] as? Set<NSManagedObject> else {
+                continue
+            }
+
+            let identifiers = objects.map { $0.objectID }
+            output.formUnion(identifiers)
+        }
+
+        return output
     }
 }
