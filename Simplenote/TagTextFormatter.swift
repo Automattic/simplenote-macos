3cf8a8f5df7ffeff24a46a6b97dbb6040d89d655
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-07-30 10:05:17 -0300

    TagTextFormatter: Bugfixes

--- Simplenote/TagTextFormatter.swift
+++ Simplenote/TagTextFormatter.swift
@@ -1,15 +1,17 @@
 import Foundation
 
 
-// MARK: - TagTextFormatter
-//         Helps us limit the Tag Length in the TagListViewController.
-//         We'll always check against the *Simperium Encoded* String
-//         (since Tag Names are hashed, and used as Tag.simperiumKey).
+// MARK: - TagTextFormatter: Helps us limit the Tag Length in the TagListViewController.
 //
 class TagTextFormatter : Formatter {
 
+    /// Maximum Allowed Tag Length
+    ///
     let maximumLength: Int
 
+    /// Designated Initializer
+    /// - Parameter maximumLength: Maximum allowed (encoded) Tag Length
+    ///
     init(maximumLength: Int) {
         self.maximumLength = maximumLength
         super.init()
@@ -28,8 +30,22 @@ class TagTextFormatter : Formatter {
         return true
     }
 
+    /// Indicates if a Partial Edition is valid:
+    /// - Note: We'll remove Attachment Markers from the input. Useful for NSTokenField usage
+    /// - Important: Maximum Tag Length is enforced against the `byEncodingAsTagHash` output string length, because
+    ///              `Tag.simperiumKey` fields are actually derived from the Tag Name.
+    ///
     override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
-        partialString.byEncodingAsTagHash.count <= maximumLength
+        guard partialString != .space else {
+            return false
+        }
+
+        let encodedString = partialString
+                                .replacingOccurrences(of: String.attachmentString, with: "")
+                                .trimmingCharacters(in: .whitespaces)
+                                .byEncodingAsTagHash
+
+        return encodedString.count <= maximumLength
     }
 
     override func attributedString(for obj: Any, withDefaultAttributes attrs: [NSAttributedString.Key : Any]? = nil) -> NSAttributedString? {
