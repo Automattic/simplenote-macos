5e6ed711a425b8acfd451e37a8de167102563378
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-08 10:52:54 -0300

    New Simplenote Constants

--- Simplenote/SimplenoteConstants.swift
+++ Simplenote/SimplenoteConstants.swift
@@ -13,4 +13,16 @@ class SimplenoteConstants: NSObject {
     /// Tag(s) Max Length
     ///
     static let maximumTagLength = 256
+
+    /// Simplenote: Scheme
+    ///
+    static let simplenoteScheme = "simplenote"
+
+    /// Simplenote: Interlink Host
+    ///
+    static let simplenoteInterlinkHost = "note"
+
+    /// Simplenote: Interlink Maximum Title Length
+    ///
+    static let simplenoteInterlinkMaxTitleLength = 150
 }
