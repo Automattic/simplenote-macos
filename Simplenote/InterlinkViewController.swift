f5b72cd387937a01b658d73444f7f977b537157a
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-25 16:13:07 -0300

    InterlinkViewController: Appearance

--- Simplenote/InterlinkViewController.swift
+++ Simplenote/InterlinkViewController.swift
@@ -6,4 +6,29 @@ import AppKit
 //
 class InterlinkViewController: NSViewController {
 
+    /// Background
+    ///
+    @IBOutlet private var backgroundView: BackgroundView!
+
+    /// Autocomplete TableView
+    ///
+    @IBOutlet private var tableView: NSTableView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBackground()
+    }
+}
+
+
+// MARK: - Setup!
+//
+private extension InterlinkViewController {
+
+    func setupBackground() {
+        backgroundView.fillColor = .simplenoteBackgroundColor
+        tableView.backgroundColor = .clear
+    }
+}
 }
