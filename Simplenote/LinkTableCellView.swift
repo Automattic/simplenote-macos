6b1d877fa0b3e2ea539424c21ad0263a48e9cb08
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-28 19:59:59 -0300

    LinkTableCellView: Highlight Support

--- Simplenote/LinkTableCellView.swift
+++ Simplenote/LinkTableCellView.swift
@@ -7,6 +7,26 @@ import AppKit
 @objcMembers
 class LinkTableCellView: NSTableCellView {
 
+    /// Workaround: In AppKit, TableView Cell Selection works at the Row level
+    ///
+    override var backgroundStyle: NSView.BackgroundStyle {
+        didSet {
+            refreshSelectedState()
+        }
+    }
+
+    /// Indicates if the receiver's associated NSTableRowView is *selected*
+    ///
+    private var selected = false {
+        didSet {
+            guard oldValue != selected else {
+                return
+            }
+
+            refreshStyle()
+        }
+    }
+
     /// Wraps access to the TextField's String Value
     ///
     var title: String? {
@@ -27,11 +47,24 @@ class LinkTableCellView: NSTableCellView {
 }
 
 
+// MARK: - Selection Workaround
+//
+private extension LinkTableCellView {
+
+    func refreshSelectedState() {
+        guard let row = superview as? NSTableRowView else {
+            return
+        }
+
+        selected = row.isSelected
+    }
+}
+
 // MARK: - Private API(s)
 //
 private extension LinkTableCellView {
 
     func refreshStyle() {
-        textField?.textColor = .simplenoteTextColor
+        textField?.textColor = selected ? .simplenoteSelectedTextColor : .simplenoteTextColor
     }
 }
