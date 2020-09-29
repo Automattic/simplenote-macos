77a02b22485fcdb77c7f286f0e408506d9f89608
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-29 16:20:56 -0300

    New NSTableVIew API

--- Simplenote/NSTableView+Simplenote.swift
+++ Simplenote/NSTableView+Simplenote.swift
@@ -27,4 +27,18 @@ extension NSTableView {
         let allColumns = IndexSet(integersIn: .zero ..< numberOfColumns)
         reloadData(forRowIndexes: selectedRowIndexes, columnIndexes: allColumns)
     }
+
+    /// Reloads the receiver's data and preserves the selected row
+    ///
+    func reloadAndPreserveSelection() {
+        let previouslySelectedRow = self.selectedRow
+        reloadData()
+
+        // Out of Bounds failsafe. Always!
+        guard previouslySelectedRow < numberOfRows else {
+            return
+        }
+
+        selectRowIndexes(IndexSet(integer: previouslySelectedRow), byExtendingSelection: false)
+    }
 }
