92c10613c783988ccbe078208568f9a825d3c277
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-10-05 18:01:10 -0300

    LookupController: Documenting API(s)

--- Simplenote/LookupController.swift
+++ Simplenote/LookupController.swift
@@ -5,8 +5,12 @@ import Foundation
 //
 class LookupController {
 
+    /// In-Memory Map of Simperium Key > Lookup Note entities. `Superfast search™`
+    ///
     private var noteMap = [String: LookupNote]()
 
+    /// Builds the LookupNote Map for a given collection of Notes
+    ///
     func preloadLookupTable(for notes: [Note]) {
         for note in notes where note.isDeleted == false {
             guard let title = note.titlePreview, title.isEmpty == false else {
@@ -17,15 +21,18 @@ class LookupController {
         }
     }
 
+    /// Returns all of the LookupNotes whose Title contain the `titleText` search keyword.
+    /// - Note: The resulting collection is ordered by Title
+    ///
     func search(titleText: String, limit: Int? = nil) -> [LookupNote] {
         let normalizedKeyword = titleText.folding(options: [.diacriticInsensitive, .caseInsensitive], locale: nil)
-
         var output = [LookupNote]()
+
         for note in noteMap.values where note.normalizedTitle.contains(normalizedKeyword) {
             output.append(note)
         }
 
-        let sorted = output.sorted { $0.normalizedTitle < $1.normalizedTitle }
+        let sorted = output.sorted { $0.title < $1.title }
         guard let limit = limit else {
             return sorted
         }
--- Simplenote/LookupNote.swift
+++ Simplenote/LookupNote.swift
@@ -5,15 +5,15 @@ import Foundation
 //
 struct LookupNote {
 
-    ///
+    /// Associated Note's Simperium Key
     ///
     let simperiumKey: String
 
-    ///
+    /// Associated Note's Title
     ///
     let title: String
 
-    ///
+    /// Diacritic and Case Insensitive Title, for matching purposes
     ///
     var normalizedTitle: String {
         title.folding(options: [.diacriticInsensitive, .caseInsensitive], locale: nil)
