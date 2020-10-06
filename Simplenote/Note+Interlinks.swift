3c4cac18787c2c13f11be5734686188b11eeabc3
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-10-05 23:09:50 -0300

    Note+Interlinks: Reverts unneeded change

--- Simplenote/Note+Interlinks.swift
+++ Simplenote/Note+Interlinks.swift
@@ -8,13 +8,12 @@ extension Note {
     /// Returns the receiver's Markdown Internal Reference, when possible
     ///
     var markdownInternalLink: String? {
-        guard let title = titlePreview, let simperiumKey = simperiumKey else {
+        guard let title = titlePreview, let key = simperiumKey else {
             return nil
         }
 
         let shortened = title.truncateWords(upTo: SimplenoteConstants.simplenoteInterlinkMaxTitleLength)
         let url = SimplenoteConstants.simplenoteScheme + "://" + SimplenoteConstants.simplenoteInterlinkHost + "/" + simperiumKey
-
         return "[" + shortened + "](" + url + ")"
     }
 }
