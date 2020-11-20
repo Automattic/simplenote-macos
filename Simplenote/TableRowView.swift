122336decce1cf97208349162fdc34a602e9d83c
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-11-20 19:43:14 -0300

    TableRowView: BigSur Style

--- Simplenote/TableRowView.swift
+++ Simplenote/TableRowView.swift
@@ -10,6 +10,23 @@ class TableRowView : NSTableRowView {
     ///
     var selectedBackgroundColor: NSColor?
 
+    /// Selection's Corner Radius
+    ///
+    var selectionCornerRadius: CGFloat = Settings.defaultCornerRadius {
+        didSet {
+            needsDisplay = true
+        }
+    }
+
+    /// Selection's Inner Selection Insets
+    ///
+    var selectionInsets: TableRowInset = .sidebar {
+        didSet {
+            needsDisplay = true
+        }
+    }
+
+
 
     // MARK: - Overridden
 
@@ -18,7 +35,32 @@ class TableRowView : NSTableRowView {
             return
         }
 
+        let insets = selectionInsets.vector
+        let targetRect = bounds.insetBy(dx: insets.dx, dy: insets.dy)
         backgroundColor.setFill()
-        NSBezierPath(rect: bounds).fill()
+        NSBezierPath(roundedRect: targetRect, xRadius: selectionCornerRadius, yRadius: selectionCornerRadius).fill()
+    }
+}
+
+
+
+private enum Settings {
+    static let defaultCornerRadius = CGFloat(6)
+}
+
+
+enum TableRowInset {
+    case sidebar
+    case list
+}
+
+extension TableRowInset {
+    var vector: CGVector {
+        switch self {
+        case .sidebar:
+            return CGVector(dx: 14, dy: .zero)
+        case .list:
+            return CGVector(dx: 12, dy: .zero)
+        }
     }
 }
