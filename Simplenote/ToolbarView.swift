8e4456cd14739631910ef6a5f1176c4593992fca
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-12-07 14:33:56 -0300

    ToolbarView: SearchBar State

--- Simplenote/ToolbarView.swift
+++ Simplenote/ToolbarView.swift
@@ -10,33 +10,24 @@ class ToolbarView: NSView {
     ///
     @IBOutlet private(set) var stackView: NSStackView!
 
-    /// Metrics Button
+    /// Buttons
     ///
     @IBOutlet private(set) var sidebarButton: NSButton!
-
-    /// Metrics Button
-    ///
     @IBOutlet private(set) var metricsButton: NSButton!
-
-    /// More Button
-    ///
     @IBOutlet private(set) var moreButton: NSButton!
-
-    /// Markdown Preview
-    ///
     @IBOutlet private(set) var previewButton: NSButton!
-
-    /// Restore Trashed Note
-    ///
     @IBOutlet private(set) var restoreButton: NSButton!
-
-    /// Search Button
-    ///
     @IBOutlet private(set) var searchButton: NSButton!
 
-    /// Search Button
+    /// Search Field
     ///
     @IBOutlet private(set) var searchField: NSSearchField!
+
+    /// Layout Constraints
+    ///
+    @IBOutlet private(set) var searchWidthConstraint: NSLayoutConstraint!
+    @IBOutlet private(set) var searchHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private(set) var searchFieldWidthConstraint: NSLayoutConstraint!
 
     /// Represents the Toolbar's State
     ///
@@ -56,6 +47,7 @@ class ToolbarView: NSView {
     override func awakeFromNib() {
         super.awakeFromNib()
         setupSubviews()
+        setupInitialSearchState()
         refreshStyle()
         startListeningToNotifications()
     }
@@ -106,7 +98,7 @@ private extension ToolbarView {
 private extension ToolbarView {
 
     var allButtons: [NSButton] {
-        [sidebarButton, metricsButton, moreButton, previewButton, restoreButton]
+        [sidebarButton, metricsButton, moreButton, previewButton, restoreButton, searchButton]
     }
 
     @objc
@@ -122,10 +114,68 @@ private extension ToolbarView {
         previewButton.toolTip = NSLocalizedString("Markdown Preview", comment: "Tooltip: Markdown Preview")
         restoreButton.toolTip = NSLocalizedString("Restore", comment: "Tooltip: Restore a trashed note")
         sidebarButton.toolTip = NSLocalizedString("Toggle Sidebar", comment: "Tooltip: Restore a trashed note")
+        searchButton.toolTip =  NSLocalizedString("Search", comment: "Tooltip: Search Action")
 
         let cells = allButtons.compactMap { $0.cell as? NSButtonCell }
         for cell in cells {
             cell.highlightsBy = .pushInCellMask
         }
     }
+
+    func setupInitialSearchState() {
+        searchFieldWidthConstraint.constant = .zero
+    }
+}
+
+
+// MARK: - Actions
+//
+extension ToolbarView {
+
+    @IBAction
+    func searchWasPressed(_ sender: Any) {
+        updateSearchBar(visible: true)
+    }
+}
+
+
+// MARK: - Search Bar State Management
+//
+private extension ToolbarView {
+
+    var isSearchBarVisible: Bool {
+        searchFieldWidthConstraint.constant != 0
+    }
+
+    func updateSearchBar(visible: Bool) {
+        let newBarAlpha     = visible ? AppKitConstants.alpha1_0 : AppKitConstants.alpha0_0
+        let newBarWidth     = visible ? Metrics.searchBarSize.width : .zero
+        let newButtonSize   = visible ? .zero : Metrics.buttonSize
+        let newButtonAlpha  = visible ? AppKitConstants.alpha0_0 : AppKitConstants.alpha1_0
+
+        NSAnimationContext.runAnimationGroup { context in
+            context.allowsImplicitAnimation = true
+            context.duration = AppKitConstants.duration0_2
+
+            searchWidthConstraint.animator().constant       = newButtonSize.width
+            searchHeightConstraint.animator().constant      = newButtonSize.height
+            searchFieldWidthConstraint.animator().constant  = newBarWidth
+
+            searchButton.animator().alphaValue = newButtonAlpha
+            searchField.animator().alphaValue  = newBarAlpha
+
+            layoutSubtreeIfNeeded()
+
+        } completionHandler: {
+            self.searchField.becomeFirstResponder()
+        }
+    }
+}
+
+
+// MARK: - Metrics
+//
+private enum Metrics {
+    static let buttonSize = CGSize(width: 22, height: 22)
+    static let searchBarSize = CGSize(width: 222, height: 29)
 }
