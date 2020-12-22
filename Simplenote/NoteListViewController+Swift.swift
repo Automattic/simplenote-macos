84ba919e523a56befa91939057dab4905f8f08d0
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-12-22 16:59:49 -0300

    NoteListViewController: Drops log statements

--- Simplenote/NoteListViewController+Swift.swift
+++ Simplenote/NoteListViewController+Swift.swift
@@ -295,19 +295,16 @@ extension NoteListViewController {
     private func refreshPresentedNote() {
         let selectedNotes = self.selectedNotes
         guard selectedNotes.count > .zero else {
-NSLog("# Display NIL")
             noteEditorViewController.displayNote(nil)
             return
         }
 
         guard selectedNotes.count == 1, let targetNote = selectedNotes.first else {
-NSLog("# Display \(selectedNotes.count)")
             noteEditorViewController.display(selectedNotes)
             return
         }
 
         SPTracker.trackListNoteOpened()
-NSLog("# Display \(targetNote.simperiumKey.debugDescription)")
         noteEditorViewController.displayNote(targetNote)
     }
 }
