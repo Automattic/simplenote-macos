ba06f878207c3e59355d56e0899589797fbf2965
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-01-05 14:38:27 -0300

    NSScrollView: Implements isScrolledToTop

--- Simplenote/NSScrollView+Simplenote.swift
+++ Simplenote/NSScrollView+Simplenote.swift
@@ -6,6 +6,14 @@ import AppKit
 //
 extension NSScrollView {
 
+    /// Indicates if we're at the upper edge of the contents
+    ///
+    var isScrolledToTop: Bool {
+        contentView.bounds.origin.y == contentView.contentInsets.top * -1
+    }
+
+    /// Scrolls the receiver to the upper edge
+    ///
     @objc(scrollToTopWithAnimation:)
     func scrollToTop(animated: Bool = false) {
         let target = NSPoint(x: .zero, y: contentView.contentInsets.top * -1)
