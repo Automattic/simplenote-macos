bb4069902a92e7b7b65d115220a6dd4522c716d6
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-01-05 15:33:44 -0300

    New Date Formatter property

--- Simplenote/DateFormatter+Simplenote.swift
+++ Simplenote/DateFormatter+Simplenote.swift
@@ -14,6 +14,15 @@ extension DateFormatter {
         return formatter
     }()
 
+    /// Date Formatter for the Notes List
+    ///
+    static let notesFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
     /// Date Formatter for History
     ///
     static let historyFormatter: DateFormatter = metricsFormatter
