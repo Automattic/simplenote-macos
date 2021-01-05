ee5afecf50296540cdbf734a8de85470a397cbc5
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-01-05 15:33:54 -0300

    Note: New Date for SortMode API

--- Simplenote/Note+Simplenote.swift
+++ Simplenote/Note+Simplenote.swift
@@ -5,6 +5,21 @@ import Foundation
 //
 extension Note {
 
+    /// Returns the Creation / Modification date for a given SortMode
+    ///
+    func date(for sortMode: SortMode) -> Date? {
+        switch sortMode {
+        case .alphabeticallyAscending, .alphabeticallyDescending:
+            return nil
+
+        case .createdNewest, .createdOldest:
+            return creationDate
+
+        case .modifiedNewest, .modifiedOldest:
+            return modificationDate
+        }
+    }
+
     /// Given a collection of Tag Names, this API will return the subset that's not already associated with the receiver.
     ///
     func filterUnassociatedTagNames(from names: [String]) -> [String] {
