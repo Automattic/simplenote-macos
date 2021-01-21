d3304ea52788c7b1f92bff326000967c59e23bb7
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-01-21 17:49:04 -0300

    AccountVerificationController: Placeholder

--- Simplenote/AccountVerificationController.swift
+++ Simplenote/AccountVerificationController.swift
@@ -8,4 +8,13 @@ struct AccountVerificationController {
     /// User's email
     ///
     let email: String
+
+
+    /// Send verification request
+    ///
+    func verify(completion: @escaping (_ success: Bool) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(true)
+        }
+    }
 }
