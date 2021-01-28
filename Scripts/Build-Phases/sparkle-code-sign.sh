1f7884510279da30db8bcf6ed6dd80e019e9e532
Gio Lodi <giovanni.lodi42@gmail.com>
2021-01-29 06:39:55 +1100

    Extract build phase for Sparkle code signing in dedicated script
    This will make it easier to update it

---
+++ Scripts/Build-Phases/sparkle-code-sign.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+# Codesign
+#
+IDENTITY="Developer ID Application: Automattic, Inc."
+SPARKLE_BIN="${SRCROOT}/External/Sparkle/bin"
+CODESIGN_TOOL="${SPARKLE_BIN}/codesign_embedded_executable"
+CODESIGN_HARDENEDRUNTIME_CMD="codesign --force -o runtime -s"
+
+# XPC
+#
+XPC_SOURCE="${SRCROOT}/External/Sparkle/XPCServices"
+XPC_TARGET="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/../XPCServices"
+
+# Services
+#
+XPC_NETWORK="org.sparkle-project.InstallerConnection.xpc"
+XPC_LAUNCHER="org.sparkle-project.InstallerLauncher.xpc"
+XPC_STATUS="org.sparkle-project.InstallerStatus.xpc"
+
+# UPDATER
+UPDATER_TARGET="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/Sparkle.framework/Versions/A/Resources"
+SPARKLE_AUTOUPDATE="Autoupdate"
+SPARKLE_UPDATER="Updater.app/Contents/MacOS/Updater"
+
+# Bail out whenever we're not in Release
+#
+if [ "${CONFIGURATION}" != "Release" ]; then
+    echo "NOT Signing in debug!"
+    exit 0
+fi
+
+# Reset: Sparkle's Codesign fails when attempting to re-sign
+#
+echo "Nuking old XPC..."
+
+rm -r "${XPC_TARGET}"
+
+# Copy Phase
+#
+echo "Copying..."
+
+mkdir "${XPC_TARGET}"
+cp -R "${XPC_SOURCE}/${XPC_NETWORK}" "${XPC_TARGET}/${XPC_NETWORK}"
+cp -R "${XPC_SOURCE}/${XPC_LAUNCHER}" "${XPC_TARGET}/${XPC_LAUNCHER}"
+cp -R "${XPC_SOURCE}/${XPC_STATUS}" "${XPC_TARGET}/${XPC_STATUS}"
+
+# Codesign Phase
+#
+echo "Signing... "
+
+${CODESIGN_TOOL} "${IDENTITY}" "${XPC_TARGET}/${XPC_NETWORK}"
+${CODESIGN_TOOL} "${IDENTITY}" "${XPC_TARGET}/${XPC_LAUNCHER}"
+${CODESIGN_TOOL} "${IDENTITY}" "${XPC_TARGET}/${XPC_STATUS}"
+${CODESIGN_HARDENEDRUNTIME_CMD} "${IDENTITY}" "${UPDATER_TARGET}/${SPARKLE_AUTOUPDATE}"
+${CODESIGN_HARDENEDRUNTIME_CMD} "${IDENTITY}" "${UPDATER_TARGET}/${SPARKLE_UPDATER}"
