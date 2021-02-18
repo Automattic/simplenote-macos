91945fb93731eb5ec3f2882d7a8b4010191f3d77
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-02-18 12:41:53 -0300

    New SortDescriptor API

--- Simplenote/NSSortDescriptor+Simplenote.swift
+++ Simplenote/NSSortDescriptor+Simplenote.swift
@@ -40,4 +40,13 @@ extension NSSortDescriptor {
     static func descriptorForPinnedNotes() -> NSSortDescriptor {
         return NSSortDescriptor(keyPath: \Note.pinned, ascending: false)
     }
+
+    /// Returns a NSSortDescriptor, to be applied over Tag collections. Yields a sorted collection of Tags, by name, ascending
+    ///
+    static func descriptorForTags() -> NSSortDescriptor {
+        let key = NSStringFromSelector(#selector(getter: Tag.name))
+        let selector = #selector(NSString.caseInsensitiveCompare)
+
+        return NSSortDescriptor(key: key, ascending: true, selector: selector)
+    }
 }
