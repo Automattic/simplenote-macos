606cdbe27358ee09529a74537e45b6694b4fd0e6
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-06-22 16:45:22 -0300

    Relocates NoteListFilter.title > TagListFilter.title

--- Simplenote/TagListFilter.swift
+++ Simplenote/TagListFilter.swift
@@ -9,3 +9,26 @@ enum TagListFilter: Equatable {
     case untagged
     case tag(name: String)
 }
+
+
+// MARK: - Public API(s)
+//
+extension TagListFilter {
+
+    /// Returns the Title matching the current state
+    ///
+    var title: String {
+        switch self {
+        case .everything:
+            return NSLocalizedString("All Notes", comment: "Title of the all notes filter")
+        case .deleted:
+            return NSLocalizedString("Trash", comment: "Title for the Trash filter")
+        case .tag(let name) where name.isEmpty:
+            return NSLocalizedString("Unnamed Tag", comment: "Title for Tag with no Name")
+        case .tag(let name):
+            return name
+        case .untagged:
+            return NSLocalizedString("Untagged", comment: "Untagged Notes Title")
+        }
+    }
+}
