e235241fe124cdffe1c9e9a82548620da9671ec4
Charlie Scheer <charlie.scheer@automattic.com>
2021-09-16 18:39:55 +1200

    Added outlets for all controls on Preferences VC

--- Simplenote/PreferencesViewController.swift
+++ Simplenote/PreferencesViewController.swift
@@ -86,4 +86,49 @@ class PreferencesViewController: NSViewController {
     ///
     @IBOutlet var aboutSimplenoteButton: NSButton!
 
+    // MARK: View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add styling code
+    }
+
+    // MARK: Account Settings
+
+    @IBAction func logOutWasPressed(_ sender: Any) {
+    }
+
+    @IBAction func deleteAccountWasPressed(_ sender: Any) {
+    }
+
+    // MARK: NoNote List Appearence Settings
+
+    @IBAction func noteSortOrderWasPressed(_ sender: Any) {
+    }
+
+    @IBAction func noteLineLengthSwitched(_ sender: Any) {
+
+    }
+
+    @IBAction func condensedNoteListPressed(_ sender: Any) {
+    }
+
+    @IBAction func sortTagsAlphabeticallyPressed(_ sender: Any) {
+    }
+
+    // MARK: Theme Settings
+
+    @IBAction func themeWasPressed(_ sender: Any) {
+    }
+
+    // MARK: Analytics Settings
+
+    @IBAction func shareAnalyticsWasPressed(_ sender: Any) {
+    }
+
+    // MARK: About Section
+
+    @IBAction func aboutWasPressed(_ sender: Any) {
+    }
+    
 }
