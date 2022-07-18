9c0fca1e1c03fd84572f6e1271e8b9d77a2720eb
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-07-18 13:44:45 -0600

    Add Buildkite

---
+++ .buildkite/commands/build-and-test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -eu
+
+# FIXIT-13.1: Temporary fix until we're on the Xcode 13.1 VM
+echo "--- :rubygems: Fixing Ruby Setup"
+gem install bundler
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :cocoapods: Setting up Pods"
+install_cocoapods
+
+echo "--- :hammer_and_wrench: Build and Test"
+bundle exec fastlane test
