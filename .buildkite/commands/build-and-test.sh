f194d6def799c045f2d8e10391b1c4843353f5f6
Gio Lodi <gio@mokacoding.com>
2026-08-05 11:03:42 +1000

    Condense verbose AI comment

--- .buildkite/commands/build-and-test.sh
+++ .buildkite/commands/build-and-test.sh
@@ -3,10 +3,8 @@
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
+# Unit tests don't need real credentials
 echo "--- :key: Providing Credentials"
-# The unit tests do not need real credentials, so take the same path the readme
-# gives external contributors. That keeps this job out of the secret store and
-# makes it a regression test for the contributor flow.
 cp Simplenote/SPCredentials.template.swift Simplenote/SPCredentials.external-contributors.swift
 
 echo "--- :hammer_and_wrench: Build and Test"
